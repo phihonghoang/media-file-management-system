7ccbad17c8d5caf54974dff3e0516f362a682dad
Phi Hong Hoang <phihonghoang@protonmail.com>
2024-04-28 17:15:02 +0200

    Finished JUnit-Tests

--- domainLogic/tst/domainLogic/administration/AudioAdminTest.java
+++ domainLogic/tst/domainLogic/administration/AudioAdminTest.java
@@ -2,39 +2,15 @@ package domainLogic.administration;
 
 import domainLogic.media.AudioImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class AudioAdminTest {
-    /*
-    public void deleteM() {
-        AudioAdmin a = new AudioAdmin();
-        a.insert(mock(Audio.class));
-        a.insert(mock(Audio.class));
-
-        List<Audio> list1 = a.list();
-
-        a.delete(null);
-
-        assertEquals(list1, a.list().size());
-
-    }
-
-    @Test
-    public void update_mockito() {
-        AudioAdmin a = new AudioAdmin();
-        Audio audio = mock(Audio.class);
-        a.insert(new AudioImpl());
-
-        //a.update("0");
-
-        //assertEquals(1,audio.getAccessCount());
-        //verify(audio).setAccessCount(1);
-    }
-     */
 
     @Test
     public void insert_notNull()
@@ -116,4 +92,5 @@ class AudioAdminTest {
         assertFalse(result);
         assertEquals(0,audio.getAccessCount());
     }
+
 }
